272bc2fd48bb821dc4b5d64b162d80c8e02c9b76
energizet <a2437@yandex.ru>
2025-09-08 01:00:18 +0300

    form feature

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,6 +4,19 @@ import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 import { OnClick } from 'src/ui/arrow-button/ArrowButton';
+import { Select } from 'src/ui/select';
+import { useState } from 'react';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 interface ArticleParamsFormProps {
 	isOpen: boolean;
@@ -14,6 +27,29 @@ export const ArticleParamsForm = ({
 	isOpen,
 	toggleOpen,
 }: ArticleParamsFormProps) => {
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	const reset = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		apply();
+	};
+
+	const apply = () => {};
+
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
@@ -21,9 +57,54 @@ export const ArticleParamsForm = ({
 				onClick={(e) => e.stopPropagation()}
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text size={22} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						onChange={setFontFamily}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						selected={fontSize}
+						name='radio'
+						onChange={setFontSize}
+						options={fontSizeOptions}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={fontColor}
+						onChange={setFontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColor}
+						onChange={setBackgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={contentWidth}
+						onChange={setContentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={reset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={apply}
+						/>
 					</div>
 				</form>
 			</aside>
